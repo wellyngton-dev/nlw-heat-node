483c9d9b54fad22c2fb5b7af6165bb3a381f9181
Wellyngton-dev <contato@wcarvalho.dev>
2021-10-24 18:49:21 -0300

    correção do cors

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,3 @@
 import { serverHttp } from "./app";
 
-serverHttp.listen(4000, () => console.log(`: Server is running on PORT 4000`))
+serverHttp.listen(4000, () => console.log(`🚀 Server is running on PORT 4000`))
